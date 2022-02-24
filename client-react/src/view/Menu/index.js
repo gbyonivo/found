f90874153634f27124dbd4c8d9c708fd68012413
gbyon <gbyonivo@gmail.com>
2022-02-24 21:12:09 +0000

    F0002-creating-frontend: adding react icons

--- client-react/src/view/Menu/index.js
+++ client-react/src/view/Menu/index.js
@@ -1,11 +1,19 @@
 import React, { useContext, useEffect } from 'react';
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet, NavLink, useNavigate } from 'react-router-dom';
+import { FaPiedPiperSquare } from 'react-icons/fa';
+import { ImExit, ImTux } from 'react-icons/im';
 import { UserContext } from '../../contexts/UserContextProvider';
 
-const navLinkClass = 'p-4';
+const navLinkClass = 'p-4 border-b border-pink-700';
+
+const menu = [
+  { to: 'accounts', Icon: ImTux, title: 'profile' },
+  { to: 'reports', Icon: FaPiedPiperSquare, title: 'reports' }
+];
 
 const Menu = () => {
   const { user, retry, setToken } = useContext(UserContext);
+  const navigate = useNavigate();
   useEffect(() => {
     if (!user) retry();
   }, [user, retry]);
@@ -13,17 +21,22 @@ const Menu = () => {
   const signOut = () => {
     localStorage.removeItem('token');
     setToken(null);
+    navigate('/');
   };
 
   if (!user) return <div>We are having issues trying to load your details.</div>
-  return <div className="flex h-screen space-between">
-    <div className="flex flex-col h-full bg-lime-800 text-white font-light justify-between pb-8 text-sm">
-      <div className="flex flex-col">
-        <NavLink to="accounts" className={navLinkClass}>My profile</NavLink>
-        <NavLink to="reports" className={navLinkClass}>Reports</NavLink>
+  return <div className="flex h-screen space-between bg-gray-200">
+    <div className="flex flex-col h-full bg-pink-900 text-white font-light justify-between pb-2 text-xs">
+      <div className="flex flex-col text-center">
+        {menu.map(({ title, to, Icon }) => <NavLink key={title} to={to} className={navLinkClass}>
+        <div className="flex justify-center mb-2"><Icon size={30} /></div>
+          {title}
+        </NavLink>
+        )}
       </div>
-      <div>
-        <NavLink to="/" className={navLinkClass} onClick={signOut}>Sign out</NavLink>
+      <div className="text-center w-full cursor-pointer flex flex-col p-4 justify-center" role="button" onClick={signOut}>
+        <div className="flex justify-center mb-2"><ImExit size={30} /></div>
+        Sign out
       </div>
     </div>
     <Outlet />
